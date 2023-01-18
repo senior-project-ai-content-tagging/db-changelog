a93fb66e86ff96155218e4a935f74375f2ed2538
thekogo <thekogo@gmail.com>
2023-01-19 02:50:57 +0700

    init project

---
+++ src/main/java/me/ponlawat/dbchangelog/DbChangelogApplication.java
@@ -0,0 +1,13 @@
+package me.ponlawat.dbchangelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbChangelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbChangelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ponlawat/dbchangelog/DbChangelogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ponlawat.dbchangelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbChangelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
